61106e40c02429d35d0f2a60c871187aa17ddd85
Anthony Staples <t-anstaples@microsoft.com>
2022-06-09 10:50:19 -0700

    Add proto Register template

---
+++ Option 2 - Register/src/functions/httpTrigger1.ps1
@@ -0,0 +1,29 @@
+#requires -module AzureFunctionsHelpers
+using namespace Microsoft.Azure.Functions.Worker
+
+param(
+    $Request,
+    $TriggerMetadata
+)
+
+# Write to the Azure Functions log stream.
+Write-Host 'PowerShell HTTP trigger function processed a request.'
+
+# Interact with query parameters or the body of the request.
+$name = $Request.Query.Name
+if (-not $name) {
+    $name = $Request.Body.Name
+}
+
+$body = 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.'
+
+if ($name) {
+    $body = "Hello, $name. This HTTP triggered function executed successfully."
+}
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+$body | Push-HttpBinding -StatusCode OK -Name Response
+
+Register-Binding -Name Request -Type HttpTrigger -Direction 'in' -AuthLevel 'anonymous' -Methods ['get', 'set']
+Register-Binding -Name Response -Type 'out' -Direction 'out' -
+Register-AzureFunction -Name 'PushHttpOutputBindingDemo' -Bindings ['Request', 'Response']
