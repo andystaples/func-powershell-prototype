ff544dbc506d59ebe4cd7b460ee06e89e36b460e
Anthony Staples <t-anstaples@microsoft.com>
2022-06-15 14:04:52 -0700

    Focus on options 1 and 4

--- Option 3B - Register - Methods/main.ps1
+++
@@ -1,8 +0,0 @@
-$app = [FunctionApp]::new()
-
-$methods = 'get', 'post'
-$bindings = 'Request', 'Response'
-
-$app.RegisterHttpBinding('Request', 'in', 'anonymous', $methods)
-$app.RegisterHttpBinding('Response', 'out')
-$app.RegisterAzureFunction('PushHttpOutputBindingDemo', $bindings)
